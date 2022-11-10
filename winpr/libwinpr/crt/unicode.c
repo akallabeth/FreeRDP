547f5fb0546c2ccd1d8e7dd4b4a312bb11f736cb
akallabeth <akallabeth@posteo.net>
2022-11-10 22:52:28 +0100

    [unicode] Fixed issues with new unicode functions
    Better handle behaviour of underlying functions

--- winpr/libwinpr/crt/unicode.c
+++ winpr/libwinpr/crt/unicode.c
@@ -372,23 +372,44 @@ SSIZE_T ConvertWCharToUtf8(const WCHAR* wstr, char* str, size_t len)
 	WINPR_ASSERT(wstr);
 
 	const int rc =
-	    WideCharToMultiByte(CP_UTF8, 0, wstr, -1, str, (int)MIN(INT32_MAX, len), NULL, NULL);
+	    int_WideCharToMultiByte(CP_UTF8, 0, wstr, -1, str, (int)MIN(INT32_MAX, len), NULL, NULL);
 	if (rc <= 0)
 		return rc;
+	else if (rc == len)
+	{
+		if (str && (str[rc - 1] != '\0'))
+			return rc;
+	}
 	return rc - 1;
 }
 
 SSIZE_T ConvertWCharNToUtf8(const WCHAR* wstr, size_t wlen, char* str, size_t len)
 {
+	BOOL isNullTerminated = FALSE;
+	if (wlen == 0)
+		return 0;
+
+	WINPR_ASSERT(wstr);
+
 	if (wlen > INT32_MAX)
 		return -1;
 
-	const int rc =
-	    WideCharToMultiByte(CP_UTF8, 0, wstr, (int)wlen, str, (int)MIN(INT32_MAX, len), NULL, NULL);
-	if (rc <= 0)
+	isNullTerminated = _wcsnlen(wstr, wlen) < wlen;
+	const int rc = int_WideCharToMultiByte(CP_UTF8, 0, wstr, (int)wlen, str,
+	                                       (int)MIN(INT32_MAX, len), NULL, NULL);
+	if ((rc <= 0) || ((len > 0) && (rc > len)))
+		return -1;
+	else if (!isNullTerminated)
+	{
+		if (str && (rc < len))
+			str[rc] = '\0';
 		return rc;
-	if (wstr && (_wcsnlen(wstr, wlen) == wlen))
-		return rc;
+	}
+	else if (rc == len)
+	{
+		if (str && (str[rc - 1] != '\0'))
+			return rc;
+	}
 	return rc - 1;
 }
 
@@ -396,21 +417,46 @@ SSIZE_T ConvertUtf8ToWChar(const char* str, WCHAR* wstr, size_t wlen)
 {
 	WINPR_ASSERT(str);
 
-	const int rc = MultiByteToWideChar(CP_UTF8, 0, str, (int)-1, wstr, (int)MIN(INT32_MAX, wlen));
+	const int iwlen = MIN(INT32_MAX, wlen);
+	const int rc = int_MultiByteToWideChar(CP_UTF8, 0, str, -1, wstr, iwlen);
 	if (rc <= 0)
 		return rc;
+	else if (iwlen == rc)
+	{
+		if (wstr && (wstr[rc - 1] != '\0'))
+			return rc;
+	}
 	return rc - 1;
 }
 
 SSIZE_T ConvertUtf8NToWChar(const char* str, size_t len, WCHAR* wstr, size_t wlen)
 {
+	BOOL isNullTerminated = FALSE;
+	if (len == 0)
+		return 0;
+
+	WINPR_ASSERT(str);
+
 	if (len > INT32_MAX)
 		return -1;
-	const int rc = MultiByteToWideChar(CP_UTF8, 0, str, (int)len, wstr, (int)MIN(INT32_MAX, wlen));
-	if (rc <= 0)
+	if (strnlen(str, len) < len)
+		isNullTerminated = TRUE;
+
+	const int iwlen = MIN(INT32_MAX, wlen);
+	const int rc = int_MultiByteToWideChar(CP_UTF8, 0, str, (int)len, wstr, (int)iwlen);
+	if ((rc <= 0) || ((wlen > 0) && (rc > iwlen)))
+		return -1;
+	if (!isNullTerminated)
+	{
+		if (wstr && (rc < iwlen))
+			wstr[rc] = '\0';
 		return rc;
-	if (str && (strnlen(str, len) == len))
-		return rc;
+	}
+	else if (rc == iwlen)
+	{
+		if (wstr && (wstr[rc - 1] != '\0'))
+			return rc;
+	}
 	return rc - 1;
 }
 
