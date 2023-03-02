94ff2adc77b4446eb70ee375e70b0e5c8eb40707
Armin Novak <anovak@thincast.com>
2023-03-02 09:43:43 +0100

    xx

--- libfreerdp/utils/passphrase.c
+++ libfreerdp/utils/passphrase.c
@@ -110,7 +110,7 @@ char* freerdp_passphrase_read(rdpContext* context, const char* prompt, char* buf
 #define CTRLC 3
 #define SHOW_ASTERISK TRUE
 
-	fwprintf(stderr, L"%s read %c [%d]", __FUNCTION__, (WCHAR)chr, chr);
+	fwprintf(stderr, L"%hs read %c [%d]", __FUNCTION__, (WCHAR)chr, chr);
 	fflush(stderr);
 			switch (chr)
 			{
