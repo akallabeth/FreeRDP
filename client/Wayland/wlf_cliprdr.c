a58ce58fecec226d94505861f0ca05a3e48b5df8
akallabeth <akallabeth@posteo.net>
2024-02-01 20:23:52 +0100

    [client,wayland] add clipboard format support
    add converters to WebP, PNG and JPEG

--- client/Wayland/wlf_cliprdr.c
+++ client/Wayland/wlf_cliprdr.c
@@ -50,12 +50,15 @@ static const char* mime_image[] = {
 	"image/png",       "image/bmp",   "image/x-bmp",        "image/x-MS-bmp",
 	"image/x-icon",    "image/x-ico", "image/x-win-bitmap", "image/vmd.microsoft.icon",
 	"application/ico", "image/ico",   "image/icon",         "image/jpeg",
-	"image/gif",       "image/tiff"
+	"image/gif",       "image/tiff",  "image/webp",         "image/png",
+	"image/jpeg"
 };
 
+static const char mime_tiff[] = "image/tiff";
 static const char mime_uri_list[] = "text/uri-list";
 static const char mime_html[] = "text/html";
-static const char mime_bmp[] = "image/bmp";
+static const char* mime_bitmap[] = { "image/bmp", "image/x-bmp", "image/x-MS-bmp",
+	                                 "image/x-win-bitmap" };
 
 static const char mime_gnome_copied_files[] = "x-special/gnome-copied-files";
 static const char mime_mate_copied_files[] = "x-special/mate-copied-files";
@@ -287,9 +290,14 @@ static BOOL wlf_cliprdr_add_client_format(wfClipboard* clipboard, const char* mi
 	}
 	else if (wlf_mime_is_image(mime))
 	{
-		UINT32 formatId = ClipboardGetFormatId(clipboard->system, mime_bmp);
-		wfl_cliprdr_add_client_format_id(clipboard, formatId);
+		for (size_t x = 0; x < ARRAYSIZE(mime_bitmap); x++)
+		{
+			const char* mime_bmp = mime_bitmap[x];
+			UINT32 formatId = ClipboardGetFormatId(clipboard->system, mime_bmp);
+			wfl_cliprdr_add_client_format_id(clipboard, formatId);
+		}
 		wfl_cliprdr_add_client_format_id(clipboard, CF_DIB);
+		wfl_cliprdr_add_client_format_id(clipboard, CF_TIFF);
 	}
 	else if (wlf_mime_is_file(mime))
 	{
@@ -539,7 +547,10 @@ static void wlf_cliprdr_transfer_data(UwacSeat* seat, void* context, const char*
 	else if (wlf_mime_is_image(mime))
 	{
 		request.responseMime = mime;
-		request.responseFormat = CF_DIB;
+		if (strcmp(mime, mime_tiff) == 0)
+			request.responseFormat = CF_TIFF;
+		else
+			request.responseFormat = CF_DIB;
 	}
 
 	if (request.responseMime != NULL)
@@ -673,16 +684,20 @@ static UINT wlf_cliprdr_server_format_list(CliprdrClientContext* context,
 
 	if (text)
 	{
-		size_t x;
-
-		for (x = 0; x < ARRAYSIZE(mime_text); x++)
+		for (size_t x = 0; x < ARRAYSIZE(mime_text); x++)
 			UwacClipboardOfferCreate(clipboard->seat, mime_text[x]);
 	}
 
 	if (image)
 	{
 		for (size_t x = 0; x < ARRAYSIZE(mime_image); x++)
-			UwacClipboardOfferCreate(clipboard->seat, mime_image[x]);
+		{
+			const char* mime = mime_image[x];
+			const UINT32 fmtId = ClipboardGetFormatId(clipboard->system, mime);
+			if (fmtId == 0)
+				continue;
+			UwacClipboardOfferCreate(clipboard->seat, mime);
+		}
 	}
 
 	UwacClipboardOfferAnnounce(clipboard->seat, clipboard, wlf_cliprdr_transfer_data,
@@ -749,7 +764,11 @@ wlf_cliprdr_server_format_data_request(CliprdrClientContext* context,
 
 		case CF_DIB:
 		case CF_DIBV5:
-			mime = mime_bmp;
+			mime = mime_bitmap[0];
+			break;
+
+		case CF_TIFF:
+			mime = mime_tiff;
 			break;
 
 		default:
