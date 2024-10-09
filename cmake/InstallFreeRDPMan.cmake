e01356693203329c1547f5d8d24f5a7796939f6a
Armin Novak <armin.novak@thincast.com>
2024-10-09 11:46:00 +0200

    xx

--- cmake/InstallFreeRDPMan.cmake
+++ cmake/InstallFreeRDPMan.cmake
@@ -34,6 +34,10 @@ function(generate_and_install_freerdp_man_from_xml target section dependencies)
 
                 cleaning_configure_file(${template}.in ${manpage}.tmp @ONLY IMMEDIATE)
 
+                # write header (aka name of the manpage), truncate existing
+                file(READ ${CMAKE_CURRENT_BINARY_DIR}/${manpage}.tmp CONTENTS)
+                file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/${manpage} "${CONTENTS}")
+
 		foreach(DEP IN LISTS dependencies)
 			get_filename_component(DNAME "${DEP}" NAME)
 			set(SRC ${CMAKE_CURRENT_SOURCE_DIR}/${DEP}.in)
@@ -44,12 +48,11 @@ function(generate_and_install_freerdp_man_from_xml target section dependencies)
                                 cleaning_configure_file(${SRC} ${DST} @ONLY IMMEDIATE)
 			else()
 				message("using ${DST} from ${SRC}")
-			endif()
-		endforeach()
+                        endif()
 
-                # write header (aka name of the manpage), truncate existing
-                file(READ ${CMAKE_CURRENT_BINARY_DIR}/${manpage}.tmp CONTENTS)
-                file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/${manpage} "${CONTENTS}")
+                        file(READ ${DST} CONTENTS)
+                        file(APPEND ${CMAKE_CURRENT_BINARY_DIR}/${manpage} "${CONTENTS}")
+		endforeach()
 
 		install_freerdp_man(${CMAKE_CURRENT_BINARY_DIR}/${manpage} ${section})
 	endif()
