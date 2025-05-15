4411bddd2ec77aa64d511efd6aacfa0f377cb108
Armin Novak <armin.novak@thincast.com>
2025-05-15 11:55:19 +0200

    xxxx

--- packaging/scripts/create_rpm.sh
+++ packaging/scripts/create_rpm.sh
@@ -4,9 +4,16 @@
 SCRIPT_PATH=$(dirname "${BASH_SOURCE[0]}")
 SCRIPT_PATH=$(realpath "$SCRIPT_PATH")
 
-mkdir -p ~/rpmbuild/SOURCES/
-$SCRIPT_PATH/prepare_rpm_freerdp-nightly.sh
+RPMBUILD_BASE=~/rpmbuild/SOURCES
+if [ $# -gt 0 ]; then
+  RPMBUILD_BASE=$1
+fi
 
-git archive --format=tar --prefix=freerdp-nightly-3.0/ HEAD --output ~/rpmbuild/SOURCES/freerdp-nightly-3.0.tar.bz2
-cp source_version ~/rpmbuild/SOURCES/
-rpmbuild -ba "$SCRIPT_PATH/../rpm/freerdp-nightly.spec"
+mkdir -p $RPMBUILD_BASE
+($SCRIPT_PATH/prepare_rpm_freerdp-nightly.sh $(pwd))
+
+git archive --format=tar --prefix=freerdp-nightly-3.0/ --output $RPMBUILD_BASE/freerdp-nightly-3.0.tar.bz2 HEAD
+cp webview.tar.bz2 $RPMBUILD_BASE/
+cp source_version $RPMBUILD_BASE/
+rpmbuild -bs "$SCRIPT_PATH/../rpm/freerdp-nightly.spec"
+rpmbuild -bb "$SCRIPT_PATH/../rpm/freerdp-nightly.spec"
--- packaging/scripts/prepare_rpm_freerdp-nightly.sh
+++ packaging/scripts/prepare_rpm_freerdp-nightly.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -xe
 
-RPMBUILD_BASE="~/rpmbuild/SOURCES"
+RPMBUILD_BASE=~/rpmbuild/SOURCES
 if [ $# -gt 0 ]; then
-  RPMBUILD_BASE="$1"
+  RPMBUILD_BASE=$1
 fi
 
 if [ ! -d external/webview ]; then
   git clone -b navigation-listener --depth=1 https://github.com/akallabeth/webview external/webview
 fi
 
+mkdir -p "$RPMBUILD_BASE"
+git rev-parse --short HEAD > $RPMBUILD_BASE/source_version
 (
   cd external/webview
-  mkdir -p "$RPMBUILD_BASE"
-  git archive --format=tar --prefix=webview/ HEAD --output $RPMBUILD_BASE/webview.tar.bz2
+  git archive --format=tar --prefix=webview/ --output $RPMBUILD_BASE/webview.tar.bz2 HEAD
 )
 
-git rev-parse --short HEAD >source_version
